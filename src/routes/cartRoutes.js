74e29e7b3ebd4151e40d184b2f16b1bc21511eb8
Gabriel <gabrielecheverria1993@gmail.com>
2024-10-22 21:45:31 -0300

    update functionality cart

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -7,11 +7,26 @@ const {
     updateCart,
     updateProductQuantity,
     clearCart,
+    getAllCarts,
 } = require('../services/cartService');
 
 const router = express.Router();
 
 router.post('/', addNewCart);
+
+router.get('/', async (req, res) => {
+    try {
+        const carts = await getAllCarts(); // Obtener los carritos desde la base de datos
+        const cartItemsCount = carts.map((cart) => ({
+            id: cart._id,
+            count: cart.products.length, // Contar los productos en cada carrito
+        }));
+        res.json(cartItemsCount); // Devolver los carritos y sus conteos en formato JSON
+    } catch (error) {
+        console.error('Error al obtener carritos:', error);
+        res.status(500).json({ message: 'Error al obtener carritos', error });
+    }
+});
 
 router.get('/:cid', getProductsByCartId);
 
